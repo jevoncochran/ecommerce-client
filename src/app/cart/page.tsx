0143d22fca954cc2f1364913c5ab35e7355110f8
Jevon Cochran <jevon@securebags.com>
2023-10-12 03:51:30 -0500

    shipping info

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,16 +1,34 @@
 "use client";
 
-import { useContext } from "react";
+import { ChangeEvent, useContext, useEffect, useState } from "react";
 import { CartContext } from "@/context/cart-context";
 import Image from "next/image";
 
 const CartPage = () => {
   const { cart } = useContext(CartContext);
 
+  const [shippingInfo, setShippingInfo] = useState({
+    name: "",
+    email: "",
+    address: "",
+    addressLine2: "",
+    city: "",
+    state: "",
+    zipCode: "",
+  });
+
+  const inputChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setShippingInfo({ ...shippingInfo, [e.target.name]: e.target.value });
+  };
+
   let totalPrice = 0;
   for (const product of cart) {
     totalPrice += product.price;
   }
+
+  useEffect(() => {
+    console.log(shippingInfo);
+  }, [shippingInfo]);
 
   return (
     <div className="grid grid-cols-12 gap-10 py-4 px-6">
@@ -60,9 +78,58 @@ const CartPage = () => {
       {!!cart.length && (
         <div className="bg-white col-span-4 rounded-lg p-7">
           <h2>Your Order</h2>
-          <input type="text" placeholder="Address" />
-          <input type="text" placeholder="Address Line 2" />
-          <button className="btn-primary">Continue to payment</button>
+          <form method="post" action="/api/checkout">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={shippingInfo.name}
+              onChange={inputChangeHandler}
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              value={shippingInfo.email}
+              onChange={inputChangeHandler}
+            />
+            <input
+              type="text"
+              placeholder="Address"
+              name="address"
+              value={shippingInfo.address}
+              onChange={inputChangeHandler}
+            />
+            <input
+              type="text"
+              placeholder="Address Line 2"
+              name="addressLine2"
+              value={shippingInfo.addressLine2}
+              onChange={inputChangeHandler}
+            />
+            <input
+              type="text"
+              placeholder="City"
+              name="city"
+              value={shippingInfo.city}
+              onChange={inputChangeHandler}
+            />
+            <input
+              type="text"
+              placeholder="State"
+              name="state"
+              value={shippingInfo.state}
+              onChange={inputChangeHandler}
+            />
+            <input
+              type="text"
+              placeholder="Zip Code"
+              name="zipCode"
+              value={shippingInfo.zipCode}
+              onChange={inputChangeHandler}
+            />
+            <button className="btn-primary">Continue to payment</button>
+          </form>
         </div>
       )}
     </div>
