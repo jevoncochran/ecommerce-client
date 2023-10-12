84d6110c46bc01a3803f06337937755e81231c34
Jevon Cochran <jevon@securebags.com>
2023-10-11 23:24:32 -0500

    CartButton

--- src/components/featured.tsx
+++ src/components/featured.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import axios from "axios";
 import Link from "next/link";
 import { Product } from "@/types";
+import CartButton from "./cart-btn";
 
 const Featured = () => {
   const [featuredProduct, setFeaturedProduct] = useState<Product | null>(null);
@@ -33,17 +34,7 @@ const Featured = () => {
           <Link href={`/products/${featuredProduct?._id}`}>
             <button className="btn-white">Read More</button>
           </Link>
-          <button className="btn-primary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-4 h-4"
-            >
-              <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
-            </svg>
-            Add to cart
-          </button>
+          <CartButton />
         </div>
       </div>
       <div className=" w-[100%] flex justify-center">
