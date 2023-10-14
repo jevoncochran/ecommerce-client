da48b6eef6e3fd9f97875358340499d0323707af
Jevon Cochran <jevon@securebags.com>
2023-10-14 00:01:34 -0500

    images on ProductPage

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -10,6 +10,15 @@ const ProductPage = ({ params }: { params: { id: string } }) => {
   const { id } = params;
 
   const [product, setProduct] = useState<Product | null>(null);
+  const [activeImage, setActiveImage] = useState(null);
+
+  const getLittleImgBorderStyling = (img: string, activeImg: string) => {
+    if (img === activeImg) {
+      return "border-2 border-gray-500";
+    } else {
+      return "border border-gray-300";
+    }
+  };
 
   useEffect(() => {
     axios.get(`/api/products/${id}`).then((res) => {
@@ -17,24 +26,49 @@ const ProductPage = ({ params }: { params: { id: string } }) => {
     });
   }, []);
 
-  return (
-    <div className="grid grid-cols-12 gap-10">
-      <WhiteContentBox additionalStyles="col-span-7">
-        <>
+  useEffect(() => {
+    setActiveImage(product?.images[0]);
+  }, [product]);
+
+  return product ? (
+    <div className="grid grid-cols-12 gap-10 py-4 px-6">
+      <WhiteContentBox additionalStyles="col-span-4">
+        <div className="flex flex-col items-center">
           <Image
-            src={product?.images[0]}
+            src={activeImage}
             alt={product?.name}
-            height={200}
-            width={200}
+            height={300}
+            width={300}
+            className="h-[300px] w-[300px]"
           />
-        </>
+          <div className="grid grid-cols-12 gap-2 mt-4">
+            {product?.images?.map((img, idx) => (
+              <div
+                key={idx}
+                className={`h-[100px] w-[100px] relative col-span-3 rounded-md box-border flex justify-center items-center ${getLittleImgBorderStyling(
+                  img,
+                  activeImage!
+                )}`}
+              >
+                <Image
+                  src={img}
+                  alt={product.name}
+                  height={100}
+                  width={100}
+                  onClick={() => setActiveImage(img)}
+                  className="h-[80px] w-[80px] object-contain"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
       </WhiteContentBox>
-      <div className="col-span-5">
+      <div className="col-span-8">
         <h1>{product?.name}</h1>
         <p>{product?.description}</p>
       </div>
     </div>
-  );
+  ) : null;
 };
 
 export default ProductPage;
--- src/components/products-grid.tsx
+++ src/components/products-grid.tsx
@@ -3,6 +3,7 @@ import { Product } from "@/types";
 import Image from "next/image";
 import CartButton from "./cart-btn";
 import { CartContext } from "@/context/cart-context";
+import Link from "next/link";
 
 interface ProductsGridProps {
   products: Product[];
@@ -10,21 +11,23 @@ interface ProductsGridProps {
 
 const ProductsGrid = ({ products }: ProductsGridProps) => {
   const { addProduct } = useContext(CartContext);
-  
+
   return (
     <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-10 mt-8">
       {products.map((product) => (
         <div key={product._id} className="flex flex-col justify-between">
-          <div className="bg-white h-[200px] relative p-5 rounded-md flex justify-center items-center">
-            {/* TODO: Have a placeholder when there is no image */}
-            <Image
-              src={product.images?.[0]}
-              alt={product.name}
-              width={150}
-              height={150}
-              className=" w-[150px] h-[150px] object-contain"
-            />
-          </div>
+          <Link href={`/products/${product._id}`}>
+            <div className="bg-white h-[200px] relative p-5 rounded-md flex justify-center items-center">
+              {/* TODO: Have a placeholder when there is no image */}
+              <Image
+                src={product.images?.[0]}
+                alt={product.name}
+                width={150}
+                height={150}
+                className=" w-[150px] h-[150px] object-contain"
+              />
+            </div>
+          </Link>
           {/* TODO: Need to figure out how to make all product name spans the same height */}
           {/* Some are larger than others depending on number of lines of text */}
           <span className="text-ellipsis mt-1 height-[100px]">
